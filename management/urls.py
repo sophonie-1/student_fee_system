e19f404e6306737ebec1b0d859e122075721aef3
bukira sophonie <bukirasophonie347@gmail.com>
2025-10-31 15:14:21 -0400

    views: i have added payment record views

--- management/urls.py
+++ management/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('students/<str:student_id>/edit/', views.StudentUpdateView.as_view(), name='student_edit'),
     path('students/<str:student_id>/delete/', views.StudentDeleteView.as_view(), name='student_delete'),
     path('fees/', views.FeeStructureListView.as_view(), name='fee_structure_list'),
+
+    path('fees/<int:pk>/update/', views.FeeStructureUpdateView.as_view(), name='fee_update'),
+    path('payments/', views.PaymentRecordView.as_view(), name='payment_record'),
 ]
+
--- management/views.py
+++ management/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DeleteView
 from django.shortcuts import redirect
-from .models import Student
-from .forms import StudentForm
+from .models import Student, Payment
+from .forms import StudentForm, PaymentForm
 from django.urls import reverse_lazy
 from django.views.generic.edit import UpdateView
 
@@ -60,6 +60,43 @@ class FeeStructureListView(LoginRequiredMixin, ListView):
         if form.is_valid():
             form.save()
             return redirect('fee_structure_list')
+        # Invalid: Manually set queryset and build context
+        self.object_list = self.get_queryset()
+        context = super().get_context_data(object_list=self.object_list, **kwargs)
+        context['form'] = form  # Include invalid form with errors
+        return self.render_to_response(context)
+
+class FeeStructureUpdateView(LoginRequiredMixin, UpdateView):
+    model = FeeStructure
+    form_class = FeeStructureForm
+    template_name = 'management/fee_update.html'
+    success_url = reverse_lazy('fee_structure_list')
+
+
+class PaymentRecordView(LoginRequiredMixin, ListView):
+    model = Payment
+    template_name = 'management/payment_record.html'
+    context_object_name = 'payments'
+    ordering = ['-date']  # Newest first
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = PaymentForm()  # Empty form for GET
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = PaymentForm(request.POST)
+        if form.is_valid():
+            payment = form.save(commit=False)  # Don't save yet
+            # Generate payment_id: P + sequential (e.g., P001)
+            last_id = Payment.objects.order_by('-payment_id').first()
+            if last_id:
+                num = int(last_id.payment_id[1:]) + 1
+                payment.payment_id = f"P{num:03d}"
+            else:
+                payment.payment_id = "P001"
+            payment.save()  # Now save with validation
+            return redirect('payment_record')
         # Invalid: Manually set queryset and build context
         self.object_list = self.get_queryset()
         context = super().get_context_data(object_list=self.object_list, **kwargs)
